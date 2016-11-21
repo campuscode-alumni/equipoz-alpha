f6ad5e419ac4f3f5f890de6ab614f23b425bbb1a
RBCampus <regisseki@gmail.com>
2016-11-21 21:10:58 -0200

    feature add equipment to contract

--- spec/factories/equipment.rb
+++ spec/factories/equipment.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :equipment do
-    description "MyString"
-    category "MyString"
-    serial_number "MyString"
+    description "Furadeira Bosch preta"
+    category "Furadeira 500w"
+    serial_number "123456789"
     acquisition_date "2016-11-17"
-    replacement_value "9.99"
+    replacement_value "500"
     usage_limit 1
   end
 end
--- spec/features/user_create_contract_spec.rb
+++ spec/features/user_create_contract_spec.rb
@@ -5,15 +5,8 @@ feature 'User creates Contract' do
 
     contract = build(:contract)
     customer = create(:customer, name: 'Campus')
-    equipment = Equipment.new(serial_number: '123456789',
-                                 description: 'Furadeira Bosch preta',
-                                 category: 'Furadeira 500w',
-                                 replacement_value: '500',
-                                 acquisition_date: '01/01/2010',
-                                 usage_limit: '8')
-
-    equipment.save
-
+    equipment = create(:equipment, description: 'Furadeira Bosch vermelha')
+    another_equipment = create(:equipment, serial_number:'CHK1245', description: 'Betoneira CSM')
 
     full_description = "#{equipment.serial_number} #{equipment.description}"
 
@@ -24,7 +17,8 @@ feature 'User creates Contract' do
     fill_in 'Prazo de Locação', with: contract.rental_period
     fill_in 'Valor Total', with: contract.total_amount
     fill_in 'Desconto', with: contract.discount
-    select full_description, from: 'Equipment'
+    check(full_description)
+    #select full_description, from: 'Equipment'
     fill_in 'Responsável', with: contract.contact
 
     click_on 'Emitir Contrato'
