6c8d4c7683f6abc13c2ba68e38859f783e5e3780
diegocaio <dbs.noronha@gmail.com>
2016-11-22 21:57:12 -0200

    receipt already issued

--- spec/features/user_create_receipt_delivery_spec.rb
+++ spec/features/user_create_receipt_delivery_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
-feature 'Users create receipt delivery' do
-  scenario "User click on 'Emitir Recibo' visit page delivery receipt" do
+feature 'Users issue receipt delivery' do
 
+  scenario "on created contract" do
+    #setup
     customer = create(:customer)
     equipment = create(:equipment)
     another_equipment = create(:equipment, serial_number: '987654')
@@ -11,10 +12,12 @@ feature 'Users create receipt delivery' do
 
     full_description = "#{equipment.serial_number} #{equipment.description}"
 
+    #teste
     visit contract_path(contract)
 
     click_on 'Emitir Recibo'
 
+    #expectativas
     expect(page).to have_css('h1', text: 'Recibo de Entrega')
     expect(page).to have_content contract.contact
     expect(page).to have_content customer.legal_name
@@ -25,5 +28,20 @@ feature 'Users create receipt delivery' do
     expect(page).to have_content contract.delivery_address
     expect(page).to have_content contract.created_at
     expect(page).to have_content contract.number
+  end
+
+  scenario "receipt already issued" do
+
+    customer = create(:customer)
+    equipment = create(:equipment)
+    another_equipment = create(:equipment, serial_number: '987654')
+    contract = create(:contract, customer: customer,
+                      equipment: [equipment, another_equipment])
+
+      visit contract_path(contract)
+
+      click_on 'Visualizar Recibo'
+
+      expect(page).to have_css('h1', text: 'Recibo de Entrega')
   end
 end
